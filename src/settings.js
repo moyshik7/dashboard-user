2ac24eb5d6fcbc9ae634328115689ce508fdf0ae
moyshik7 <moyshik7@gmail.com>
2021-06-25 15:37:35 +0600

    Added api link

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
-var apiURL = "http://10.137.195.138:8080/";
+var apiURL = "https://dashboard-1.3c3bfbb28644.repl.co/";
 var oauthURL =
     "https://discord.com/api/oauth2/authorize?client_id=748160981766635540&redirect_uri=https%3A%2F%2Fdashboard.plubin.xyz%2Flogin&response_type=code&scope=identify%20guilds";
