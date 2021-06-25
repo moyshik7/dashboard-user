3bf9efe873b873422a650a030e28cb40ffc63d81
moyshik7 <moyshik7@gmail.com>
2021-06-25 13:49:06 +0600

    changed oauth2 link

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 var apiURL = "http://10.137.195.138:8080/";
 var oauthURL =
-    "https://discord.com/api/oauth2/authorize?client_id=748160981766635540&redirect_uri=http%3A%2F%2F10.137.195.138%3B8000%2Flogin&response_type=code&scope=identify%20guilds";
+    "https://discord.com/api/oauth2/authorize?client_id=748160981766635540&redirect_uri=https%3A%2F%2Fdashboard.plubin.xyz%2Flogin&response_type=code&scope=identify%20guilds";
