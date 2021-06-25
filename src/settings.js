0a5252b0f626200949abfb8b8b2f3b5ab14278e4
moyshik7 <moyshik7@gmail.com>
2021-06-25 20:00:25 +0600

    Fuq repl I'm moving to qovery

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
-var apiURL = "https://dashboard-1.3c3bfbb28644.repl.co/";
+var apiURL = "https://main-dashboa-bbax9i3k75v7jdwu-gtw.qovery.io/";
 var oauthURL =
     "https://discord.com/api/oauth2/authorize?client_id=748160981766635540&redirect_uri=https%3A%2F%2Fdashboard.plubin.xyz%2Flogin&response_type=code&scope=identify%20guilds";
